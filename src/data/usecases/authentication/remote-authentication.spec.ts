c2f809569398aba289cc1821bb32ba81c5916147
Andre Morita <andreluizmorita@gmail.com>
2020-10-10 15:06:45 -0300

    refactor: move SUT cration to factory

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,24 +1,28 @@
 import { HttpPostClient } from '@/data/protocols/http/http-post-client'
+import { HttpPostClientSpy } from '@/data/test/mock-http-client'
 import { RemoteAuthentication } from './remote-authentication'
 
+type SutTypes = {
+  sut: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string = 'any_url'): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  // sut = system under testing
+  const sut = new RemoteAuthentication(url, httpPostClientSpy)
+
+  return {
+    sut,
+    httpPostClientSpy
+  }
+}
+
 describe('RemoteAuthentication', () => {
-  test('Should call HttpClient with correct URL', () => {
-    class HttpPostClientSpy implements HttpPostClient {
-      url?: string
-
-      async post (url: string): Promise<void> {
-        this.url = url
-
-        return Promise.resolve()
-      };
-    }
-
-    const url = 'any_url'
-    const httpPostClientSpy = new HttpPostClientSpy()
-    // sut = system under testing
-    const sut = new RemoteAuthentication(url, httpPostClientSpy)
-
-    sut.auth()
+  test('Should call HttpClient with correct URL', async () => {
+    const url = 'other_url'
+    const { sut, httpPostClientSpy } = makeSut(url)
+    await sut.auth()
     expect(httpPostClientSpy.url).toBe(url)
   })
 })
