e2e20dd7bfa1372b7114e43012f052de74a63a8a
Andre Morita <andreluizmorita@gmail.com>
2020-10-11 13:20:41 -0300

    refactor: add generic http post client

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -7,14 +7,16 @@ import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-err
 import { mockAuthentication } from '@/domain/test/mock-authentication'
 import { RemoteAuthentication } from './remote-authentication'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
+import { AuthenticationParams } from '@/domain/usecases/authentication'
+import { AccountModel } from '@/domain/models/account-model'
 
 type SutTypes = {
   sut: RemoteAuthentication
-  httpPostClientSpy: HttpPostClientSpy
+  httpPostClientSpy: HttpPostClientSpy<AuthenticationParams, AccountModel>
 }
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy()
+  const httpPostClientSpy = new HttpPostClientSpy<AuthenticationParams, AccountModel>()
   // sut = system under testing
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
 
