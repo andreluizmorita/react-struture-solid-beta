009ee5e574cb9230db4f37d883f2eb0119ef9990
Andre Morita <andreluizmorita@gmail.com>
2020-10-12 09:36:13 -0300

    chore: axios

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,18 +1,28 @@
+import { HttpPostParams } from '@/data/protocols/http'
 import axios from 'axios'
 import faker from 'faker'
 
 import { AxiosHttpClient } from './axios-http-client'
 
 jest.mock('axios')
-
 const mockedAxios = axios as jest.Mocked<typeof axios>
 
-describe('AxiosHttpClient', () => {
-  test('Should call axios with correct URL', async () => {
-    const url = faker.internet.url()
-    const sut = new AxiosHttpClient()
-    await sut.post({ url })
+const makeSut = (): AxiosHttpClient => {
+  return new AxiosHttpClient()
+}
 
-    expect(mockedAxios).toHaveBeenCalledWith(url)
+const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.random.objectElement
+})
+
+describe('AxiosHttpClient', () => {
+  test('Should call axios with correct values', async () => {
+    const request = mockPostRequest()
+    const sut = makeSut()
+
+    await sut.post(request)
+
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
   })
 })
